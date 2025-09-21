966d296cea89cf071e4390a8d20ea00ee6ec0c24
rishi <pandeyrishiv@gmail.com>
2025-09-21 18:11:56 +0100

    feat(root):mongo client implementation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import pymongo
 
 app = FastAPI()
+mongoClient = pymongo.MongoClient("mongodb+srv://pandeyrishiv_db_user:HRishiPandey06!@cluster0.jphkthz.mongodb.net/")
+mongoDb = mongoClient["admin"]
 
 @app.get("/")
 def read_root():
