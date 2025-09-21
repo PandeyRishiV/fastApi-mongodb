9bf8f7531d69d8bbf1bad7d256b3b8a4d52401ca
rishi <pandeyrishiv@gmail.com>
2025-09-21 20:48:03 +0100

    feat: add collection

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from pydantic import BaseModel
 import pymongo
 
 app = FastAPI()
+
+# Mongo DB connection, db and collection
 mongoClient = pymongo.MongoClient("mongodb+srv://pandeyrishiv_db_user:HRishiPandey06!@cluster0.jphkthz.mongodb.net/")
 mongoDb = mongoClient["admin"]
+collection = mongoDb["users"]
 
 @app.get("/")
 async def read_root():
-    return {"Hello" : mongoClient.list_database_names()}
+    return {"Hello" : mongoDb.list_collection_names()}
