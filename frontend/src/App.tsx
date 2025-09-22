23f78a4f2757a404fd3cee516951078d9104e8a1
rishi <pandeyrishiv@gmail.com>
2025-09-22 15:38:03 +0100

    feat(frontend): generate react files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome name="Alphis" color="cyan"></Welcome>
+    </>
+  );
+}
+
+export default App;
