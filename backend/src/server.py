0e32f2e4388ee0dc58f019de5135b43cffc0b7c7
rishi <pandeyrishiv@gmail.com>
2025-09-23 20:12:16 +0100

    feat(backend): fetch and create todo list apis

--- backend/src/server.py
+++ backend/src/server.py
@@ -28,3 +28,28 @@ async def lifespan(app: FastAPI):
     client.close()
 
 app = FastAPI(lifespan=lifespan, debug=DEBUG)
+
+@app.get("/api/lists")
+async def get_all_lists() -> list[ListSummary]:
+    return [i async for i in app.todo_dal.list_todo_lists()]
+
+class NewList(BaseModel):
+    name: str
+
+class NewListResponse(BaseModel):
+    id: str
+    name: str
+
+@app.post("/api/lists",status_code=status.HTTP_201_CREATED)
+async def create_todo_list(new_list: NewList) -> NewListResponse:
+    return NewListResponse(
+        id=app.todo_dal.create_todo_list(new_list.name),
+        name=new_list.name
+    )
+
+@app.get("/api/lists/{id}")
+async def get_list(id: str) -> ToDoList:
+    return app.todo_dal.get_todo_list(id)
+
+# @app.delete("/api/lists/{id}")
+# async def delete_list()
